baa65490adc9aa5ebde35f83faea2bec466bac3c
lhagan <luke@lukehagan.com>
2012-06-08 20:04:50 -0400

    add StartPage support

--- SearchExclude.safariextension/script.js
+++ SearchExclude.safariextension/script.js
@@ -6,12 +6,12 @@
 // based on a user script by JRice
 // http://userscripts.org/scripts/show/9462
 
-var regex = /experts-exchange\.com|ehow\.com|ehow\.co\.uk|answers\.yahoo\.com|ask\.com/g;
+var regex = /experts-exchange\.com|ehow\.com|ehow\.co\.uk|answers\.yahoo\.com|ask\.com|plus\.google\.com/g;
 var list = document.getElementsByTagName('a');
 for (i=0; i<list.length; i++) {
 	var item = list[i];
   	if(regex.test(item)) {
-    	if (item.className == "l") {
+    	if (item.className == "l" || item.parentNode.parentNode.className == "result") {
 			item.parentNode.parentNode.style.display = 'none';
 		}
 	}
